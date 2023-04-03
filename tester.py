61c71589059a05cc7374d11e0ca7bb722080f0ef
Leo <ulloaferrerleonardo@gmail.com>
2023-04-02 22:57:35 -0400

    Finished right_gen

--- tester.py
+++ tester.py
@@ -1,18 +1,18 @@
 from optimal_solver import optimal_solver
 from slow_solver import slow_solver
-from random_generator import random_generator 
+from random_generator import random_generator, random_right
 from colorama import Fore, Back, Style
 from random import randint
 import json
 import os.path
 
-def gen_cases(count,s_len = None, t_len = None):
+def gen_cases(count,rand_func = random_generator,s_len = None, t_len = None, file_path = 'test.txt' ):
     to_save = []
-    if os.path.exists('test.txt') and os.path.isfile('test.txt'):
-        with open('test.txt','r') as fin:
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        with open(file_path,'r') as fin:
             to_save = json.load(fin)
     for i in range(count):
-        S,T = random_generator(s_len,t_len)
+        S,T = rand_func(s_len,t_len)
         print(f'>>>> case {i + 1}')
         print(f'S = {S}')
         print(f'T = {T}')
@@ -21,9 +21,9 @@ def gen_cases(count,s_len = None, t_len = None):
         to_save.append({'s': S, 't' : T, 'result': result})
         if(i % 10 == 0):
             print(f'{i + 1} cases saved >>>>')
-            with open('test.txt', 'w') as file:
+            with open(file_path, 'w') as file:
                 json.dump(to_save,file)
-    with open('test.txt', 'w') as file:
+    with open(file_path, 'w') as file:
         json.dump(to_save,file)
         
 def json_tester(file_path = 'test.txt'):
@@ -32,16 +32,32 @@ def json_tester(file_path = 'test.txt'):
         with open(file_path,'r') as fin:
             to_save = json.load(fin)
     wrong_answer = []
+    cant_solo_dcha = 0
+    cant_solo_izda = 0
+    cant_izda = 0
     cant_dcha = 0
+    cant_izda_dcha = 0
+    cant_zero = 0
     for case in to_save:
         S = case['s']
         T = case['t']
         # print(f'Caso S = {S} y T = {T}')
         real_value = case['result']
+        if(real_value == 0):
+            cant_zero += 1
         # print(f'real_value = {real_value}')
-        actual_value, dcha = optimal_solver(S, T)
+        actual_value, dcha, izda = optimal_solver(S, T)
         if(dcha):
             cant_dcha += 1
+            if (not izda):
+                cant_solo_dcha += 1
+            else:
+                cant_izda_dcha += 1
+        if(izda):
+            cant_izda += 1
+            if (not dcha):
+                cant_solo_izda += 1
+        
         # print(f'actual value = {actual_value}')
 
         if real_value != actual_value:
@@ -72,71 +88,57 @@ def json_tester(file_path = 'test.txt'):
 
     print(f'accuracy: {(len(to_save) - len(wrong_answer))/len(to_save)}')
     print(f'right_rat: {cant_dcha/len(to_save)}')
+    print(f'left_rat: {cant_izda/len(to_save)}')
+    print(f'only_right_rat: {cant_solo_dcha/len(to_save)}')
+    print(f'only_left_rat: {cant_solo_izda/len(to_save)}')
+    print(f'left_and_right_rat: {cant_izda_dcha/len(to_save)}')
+    print(f'zero_rat: {cant_zero/len(to_save)}')
+
     print(f'right_accuracy: {(cant_dcha - cant_wrong_right)/cant_dcha}')
 
         
     
 
 
-# def tester(cases_count :int, str_lenght :int):
-#     wrong_answer = []
-#     cant_dcha = 0
-#     for i in range(cases_count):
-#         S, T = random_generator(str_lenght)
-#         print(f'Caso S = {S} y T = {T}')
-#         actual_value, dcha = optimal_solver(S, T)
-#         if(dcha):
-#             cant_dcha += 1
-#         print(f'actual value = {actual_value}')
-#         real_value = slow_solver(S, T, False)
-#         print(f'real_value = {real_value}')
+def tester(cases_count :int, str_lenght :int):
+    wrong_answer = []
+    cant_dcha = 0
+    for i in range(cases_count):
+        S, T = random_generator(str_lenght)
+        print(f'Caso S = {S} y T = {T}')
+        actual_value, dcha = optimal_solver(S, T)
+        if(dcha):
+            cant_dcha += 1
+        print(f'actual value = {actual_value}')
+        real_value = slow_solver(S, T, False)
+        print(f'real_value = {real_value}')
         
-#         with open("tests.txt", "w") as f:
-#             f.write(f"S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}\n")
+        with open("tests.txt", "w") as f:
+            f.write(f"S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}\n")
 
-#         if real_value != actual_value:
-#             print(Fore.RED + f"Error: S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}")
-#             print(Fore.WHITE)
-#             wrong_answer.append({'s':S,'t':T,'actual':actual_value,'real':real_value,'dcha':dcha})
-#         else:
-#             print(Fore.GREEN + f"S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}\n")
-#             print(Fore.WHITE)
+        if real_value != actual_value:
+            print(Fore.RED + f"Error: S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}")
+            print(Fore.WHITE)
+            wrong_answer.append({'s':S,'t':T,'actual':actual_value,'real':real_value,'dcha':dcha})
+        else:
+            print(Fore.GREEN + f"S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}\n")
+            print(Fore.WHITE)
 
-#     print('SUMARY:')
-#     print('wrong cases:')
-#     for case in wrong_answer:
-#         S = case['s']
-#         T = case['t']
-#         actual_value = case['actual']
-#         real_value = case['real']
-#         dcha = case['dcha']
-#         print(Fore.RED + f"Error: S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}, dcha = {dcha}")
-#         print(Fore.WHITE)
-#     print(f'accuracy: {len(wrong_answer)/cases_count}')
-#     print(f'right_rat: {dcha}')
-
-
-
-    
-        
-
-# S = 'srr'
-# T = 'rsr'
-
-# print(slow_solver(S,T,True))
-# print(optimal_solver(S,T)[0])
+    print('SUMARY:')
+    print('wrong cases:')
+    for case in wrong_answer:
+        S = case['s']
+        T = case['t']
+        actual_value = case['actual']
+        real_value = case['real']
+        dcha = case['dcha']
+        print(Fore.RED + f"Error: S = {S}, T = {T}, Real Value = {real_value}, Actual Value = {actual_value}, dcha = {dcha}")
+        print(Fore.WHITE)
+    print(f'accuracy: {len(wrong_answer)/cases_count}')
+    print(f'right_rat: {dcha}')
 
 
 
-# tester(100,20)
-# temp = [{'s': 'Pepe','number': 15 },{'s': 'Maria','number': 55 },{'s': 'Leo','number': 200}]
 
-# with open('tests.txt','w') as fout:
-#     json.dump(temp,fout)
-
-
-json_tester()
-
-# gen_cases(2000)
 
 
